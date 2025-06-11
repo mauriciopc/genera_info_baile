c87551912413acad0250f31e3cdbae8ee90587cb
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-11 00:47:52 -0600

    Se vuelve a actualizar linea para quitar la ruta del binario de chrome

--- genera_informacion.py
+++ genera_informacion.py
@@ -18,7 +18,6 @@ def inicializa_driver():
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.binary_location = "/snap/bin/chromium"
 
     driver = webdriver.Chrome(options=options)
 
