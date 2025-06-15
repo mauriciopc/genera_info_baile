a21015ed3cf7033687b78bc845ca66177b420710
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-14 22:49:11 -0600

    Se actualiza la forma de traerse el domicilio

--- genera_informacion.py
+++ genera_informacion.py
@@ -84,7 +84,7 @@ def obtiene_informacion(driver,infoUrl):
 
             try:
                 # Obtener domicilio
-                domicilio = driver.find_element(By.XPATH, '//span[contains(@class, "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x1lkfr7t x1lbecb7 xo1l8bm xi81zsa x1yc453h")]//span').text
+                domicilio = driver.find_element(By.XPATH, '//div[contains(@class, "x78zum5 xdt5ytf x1wsgfga x9otpla")]//div[3]//span//span').text
                 print("Esto es lo que tiene el domicilio:",domicilio )
             except Exception as e:
                 print("Existio un problema al obtener el domicilio,",e)
