89d041f5a36272a1c7897c279091ac440d5bd181
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-25 09:33:02 -0600

    Se coloca print para saber cuando no cuentra un evento

--- genera_informacion.py
+++ genera_informacion.py
@@ -46,6 +46,7 @@ def obtiene_informacion(driver,infoUrl):
     #Se valida que existen eventos proximos para la pagina
     validaEventos = driver.find_elements(By.XPATH, "//a[.//span[text()='Upcoming']]")
     if(not(validaEventos)):
+       print("No se encontraron eventos para: ",url, flush=True)
        return {}
     
     #Se incializa variable para guardar informacion de los eventos
