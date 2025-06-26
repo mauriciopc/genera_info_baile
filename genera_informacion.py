88e405109ed3de3d15722bc0ac0bfbf016fa0597
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-25 22:02:20 -0600

    Se sube configuración para el headless

--- genera_informacion.py
+++ genera_informacion.py
@@ -25,7 +25,7 @@ def inicializa_driver():
 
     # Configuración del navegador
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
