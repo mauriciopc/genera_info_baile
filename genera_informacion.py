1b18ba492f98b20bcab5a8b3e40beba09aa6d3bc
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-26 23:24:09 -0600

    Se agrega disable-gpu

--- genera_informacion.py
+++ genera_informacion.py
@@ -26,6 +26,7 @@ def inicializa_driver():
     # Configuración del navegador
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
