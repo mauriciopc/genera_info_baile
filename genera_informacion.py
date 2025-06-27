428ff9f6e7d1e459e1b8b5e2c097ac8da782a111
Mauricio Perez <mauricioperez1992@gmail.com>
2025-06-26 23:27:35 -0600

    se quita lo del gpu

--- genera_informacion.py
+++ genera_informacion.py
@@ -26,7 +26,6 @@ def inicializa_driver():
     # Configuración del navegador
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
-    options.add_argument("--disable-gpu")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
